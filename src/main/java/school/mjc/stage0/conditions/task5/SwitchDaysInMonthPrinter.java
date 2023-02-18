69df2c792c013188001edc8053be202e2d822496
epampam <epampam@wp.pl>
2023-02-18 23:18:08 +0100

    finished

--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
+        int days = switch (month) {
+            case 2 -> 28;
+            case 4, 6, 9, 11 -> 30;
+            case 1, 3, 5, 7, 8, 10, 12 -> 31;
+            default -> -1;
+        };
+        System.out.println(days < 0 ? "wrong number!" : days);
     }
 }
